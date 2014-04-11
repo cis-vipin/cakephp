2a3c8c17606a1fa2b01054308c871f437616e59b
Vipin <vipin.s@cisinlabs.com>
2014-04-11 11:59:50 +0530

    user login

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -3,6 +3,25 @@
 class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
+    
+    public function isAuthorized($user) {
+    // All registered users can add posts
+    if ($this->action === 'add') {
+        return true;
+    }
+
+    // The owner of a post can edit and delete it
+    if (in_array($this->action, array('edit', 'delete'))) {
+        $postId = (int) $this->request->params['pass'][0];
+        if ($this->Post->isOwnedBy($postId, $user['id'])) {
+            return true;
+        }
+    }
+
+    return parent::isAuthorized($user);
+}
+
+
     public function index() {
         $this->set('posts', $this->Post->find('all'));
     }
@@ -20,14 +39,14 @@ class PostsController extends AppController {
     }
 
     public function add() {
-        if ($this->request->is('post')) {
-            $this->Post->create();
-            if ($this->Post->save($this->request->data)) {
-                $this->Session->setFlash(__('Your post has been saved.'));
-                return $this->redirect(array('action' => 'index'));
-            }
-            $this->Session->setFlash(__('Unable to add your post.'));
+    if ($this->request->is('post')) {
+        //Added this line
+        $this->request->data['Post']['user_id'] = $this->Auth->user('id');
+        if ($this->Post->save($this->request->data)) {
+            $this->Session->setFlash(__('Your post has been saved.'));
+            return $this->redirect(array('action' => 'index'));
         }
+    }
     }
 
     public function edit($id = null) {
